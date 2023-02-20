ade17d42bfd91e289bfc6be4a659d652ca0fe51e
gkalavala <115970675+gkalavala@users.noreply.github.com>
2023-02-20 14:40:04 +0530

    Update HelloController.java

--- gs-spring-boot-master/app/src/main/java/hello/HelloController.java
+++ gs-spring-boot-master/app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "This is java based application!";
+        return "This is java based application uses in maven!";
     }   
 }
